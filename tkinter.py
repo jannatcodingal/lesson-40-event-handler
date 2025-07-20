ae19fdc717b0ad6e705939cb1840fc1345661f33
jannatcodingal <jannatrehman007@gmail.com>
2025-07-20 21:36:39 +0500

    virus detect

--- tkinter.py
+++ tkinter.py
@@ -1,14 +1,9 @@
 from tkinter import *
-window=Tk()
-window.title('Event Handler')
-window.geometry('100x100')
-def handle_keypress(event):
-    """Print the character associated to the key pressed"""
-    print(event.char)
-    window.bind("<key>", handle_keypress)
-def handle_click(event):
-    print("\nThe button was clicked")
-button=Button(text="click me!")
-button.pack()
-button.bind("<Button-1>",handle_click)
-window.mainloop()
+from tkinter import messagebox
+root=Tk()
+root.geometry('200x200')
+def msg():
+    messagebox.showwarning("Alert!","stop! virus found")
+button=Button(root, text="scan for virus",command=msg)
+button.place(x=40,y=80)
+root.mainloop()
